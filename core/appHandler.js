9b17e5ae55a6bf0ec8ba41c25956c26e6e62badd
Subash <accounts@vuln.in>
2017-11-23 14:42:28 +0530

    Fix #6 Calc error handling

--- core/appHandler.js
+++ core/appHandler.js
@@ -241,14 +241,20 @@ module.exports.redirect = function (req, res) {
 
 module.exports.calc = function (req, res) {
 	if(vh.vEqn(req.body.eqn)){
-		if (req.body.eqn) {
-			res.render('app/calc', {
-				output: mathjs.eval(req.body.eqn)
-			})
-		} else {
+		try{
+			if (req.body.eqn) {
+				res.render('app/calc', {
+					output: mathjs.eval(req.body.eqn)
+				})
+			} else {
+				res.render('app/calc', {
+					output: 'Enter a valid math string like (3+3)*2'
+				})
+			}
+		}catch(err){
 			res.render('app/calc', {
 				output: 'Enter a valid math string like (3+3)*2'
-			})
+			})				
 		}		
 	}else{
 		res.render('app/calc', {
