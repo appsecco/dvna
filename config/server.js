ee79d1ce8f1dc50840f2442bee1172e6ec858caf
benjaminstokes <stokesbo@gmail.com>
2019-01-23 14:01:32 -0600

    Update server.js

--- config/server.js
+++ config/server.js
@@ -2,3 +2,5 @@ module.exports = {
     listen: process.env.APP_LISTEN || '0.0.0.0',
     port: process.env.APP_PORT || process.env.PORT || 9090
 }
+
+console.log('making changes for incremental scan triggering');
