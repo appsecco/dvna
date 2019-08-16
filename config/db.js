fde911975f45feece568d2672e63ef0b9b8090fa
Rudy Marquez <rudy.marquez@checkmarx.com>
2019-08-15 18:57:09 -0700

    removed invalid password fields being reported.

--- config/db.js
+++ config/db.js
@@ -2,6 +2,7 @@ module.exports = {
   username: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
+  database2: process.env.MYSQL_PASS,  //should trigger another FP
   host: process.env.MYSQL_HOST || 'mysql-db',
   port: process.env.MYSQL_PORT || 3306,
   dialect: 'mysql'
