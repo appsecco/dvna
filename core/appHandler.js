e5620828e31f8f4f0af3b9217e1f54dbc2af4000
Bassmalla Hafez <36860931+bassmalla@users.noreply.github.com>
2019-12-10 15:37:00 +0200

    Update appHandler.js

--- core/appHandler.js
+++ core/appHandler.js
@@ -245,4 +245,32 @@ module.exports.bulkProducts =  function(req, res) {
 	}else{
 		res.render('app/bulkproducts',{messages:{danger:'Invalid file'},legacy:false})
 	}
+	module.exports.userSearch = function (req, res) {
+	var query = "SELECT name,id FROM Users WHERE login='" + req.body.login + "'";
+	db.sequelize.query(query, {
+		model: db.User
+	}).then(user => {
+		if (user.length) {
+			var output = {
+				user: {
+					name: user[0].name,
+					id: user[0].id
+				}
+			}
+			res.render('app/usersearch', {
+				output: output
+			})
+		} else {
+			req.flash('warning', 'User not found')
+			res.render('app/usersearch', {
+				output: null
+			})
+		}
+	}).catch(err => {
+		req.flash('danger', 'Internal Error')
+		res.render('app/usersearch', {
+			output: null
+		})
+	})
+}
 }
